4a0bdb67ed0a1bcbfff7ab34f923bcbc1d43811a
DIEGOHORVATTI <d.horvattid@gmail.com>
2024-04-03 22:57:52 -0300

    Add database connection and team data display to lol.php

---
+++ lol.php
@@ -0,0 +1,19 @@
+<?php require_once('Connections/banco1.php'); ?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
+</head>
+
+<body>
+  <h2>Dados da Equipe:</h2>
+
+  <h4>kapa 12</h4>
+
+</body>
+
+</html>
